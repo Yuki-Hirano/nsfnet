43104f0b0daf32a0bd4a9836861958b7ff42b11d
yhirano <hiyu11vtr.f@gmail.com>
2018-05-23 10:43:44 +0900

    At *_2, probability 0.01 -> 0.1

--- conventional2B_2.sh
+++ conventional2B_2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-gcc -Wall SA_main_gen2B.c -lm;./a.out 0.01 0.05 0.988 1000;
-gcc -Wall SA_main_gen2B.c -lm;./a.out 0.01 0.05 0.9975 5000;
-gcc -Wall SA_main_gen2B.c -lm;./a.out 0.01 0.05 0.9987 10000;
-gcc -Wall SA_main_gen2B.c -lm;./a.out 0.01 0.05 0.9994 20000;
+gcc -Wall SA_main_gen2B.c -lm;./a.out 0.1 0.05 0.988 1000;
+gcc -Wall SA_main_gen2B.c -lm;./a.out 0.1 0.05 0.9975 5000;
+gcc -Wall SA_main_gen2B.c -lm;./a.out 0.1 0.05 0.9987 10000;
+gcc -Wall SA_main_gen2B.c -lm;./a.out 0.1 0.05 0.9994 20000;
--- discrete2B_2.sh
+++ discrete2B_2.sh
@@ -2,7 +2,7 @@
 
 
     
-gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.01 0.05 0.988 1000;
-gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.01 0.05 0.9975 5000;
-gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.01 0.05 0.9987 10000;
-gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.01 0.05 0.9994 20000;
+gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.1 0.05 0.988 1000;
+gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.1 0.05 0.9975 5000;
+gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.1 0.05 0.9987 10000;
+gcc -Wall SA_main_discrete2B.c -lm;./a.out 0.1 0.05 0.9994 20000;
